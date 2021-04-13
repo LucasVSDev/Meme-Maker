627300566739b4b5c0676d58a213b78147f7e2a0
lucas ramos <lucasramos1418@gmail.com>
2021-04-13 02:16:24 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './pages/Home';
+
+import GlobalStyles from './styles/global';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Home />
+    </>
+  )
+}
+
+export default App
